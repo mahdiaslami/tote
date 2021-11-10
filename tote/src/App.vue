cf0f2bb61dad7c742c24bbe11f93d3620872c7d0
mahdyaslami <mahdyaslami@gmail.com>
2021-11-10 06:22:08 +0330

    Move old app.

--- tote/src/App.vue
+++ tote/src/App.vue
@@ -1,26 +1,259 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div
+    v-for="todo in todos"
+    :key="todo.id"
+    class="task"
+  >
+    <input
+      class="tick"
+      type="checkbox"
+      :checked="todo.completed"
+      @change="complete(todo.id)"
+    >
+    <span
+      class="content"
+      :class="[
+        todo.completed ?
+          'bold linethrough' : '',
+        (todo.id === currentId) ? 'underline' : ''
+      ]"
+      dir="auto"
+      @click="() => {select(todo.id); return false;}"
+    >
+      {{ todo.text }}
+    </span>
+    <button
+      class="btn btn-delete"
+      @click="deleteTodo(todo.id)"
+    >
+      ✗
+    </button>
+  </div>
+
+  <div class="form">
+    <div class="title-input">
+      <div class="image">
+        <img
+          src="/writing-hand.gif"
+          alt="writing-hand"
+        >
+      </div>
+
+      <input
+        v-model="newTodo"
+        type="text"
+        dir="auto"
+        @keyup.enter="saveTodo"
+      >
+    </div>
+
+    <button
+      class="btn btn-save"
+      @click="saveTodo"
+    >
+      Save
+    </button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      newTodo: '',
+      todos: this.getFromLocalStorage() ?? [],
+      currentId: -1,
+    };
+  },
+
+  watch: {
+    todos() {
+      this.saveToLocalStorage();
+    },
+  },
+
+  created() {
+    this.database = document.location.pathname.split('/').pop();
+    document.title = `Todo: ${this.database}`;
+  },
+
+  methods: {
+    select(id) {
+      if (this.currentId === id) {
+        this.currentId = -1;
+      } else {
+        this.currentId = id;
+      }
+    },
+
+    saveTodo() {
+      this.todos.push(this.createTodo());
+      this.newTodo = '';
+    },
+
+    createTodo() {
+      return {
+        id: new Date().getTime(),
+        text: this.newTodo,
+        completed: false,
+      };
+    },
+
+    complete(id) {
+      const todo = this.todos.find((item) => item.id === id);
+      todo.completed = !todo.completed;
+      this.saveToLocalStorage();
+    },
+
+    deleteTodo(id) {
+      const index = this.todos.findIndex((todo) => todo.id === id);
+      this.todos.splice(index, 1);
+    },
+
+    saveToLocalStorage() {
+      localStorage.setItem(this.database, JSON.stringify(this.todos));
+    },
+
+    getFromLocalStorage() {
+      return JSON.parse(localStorage.getItem(this.database));
+    },
   },
 };
 </script>
 
 <style>
+* {
+    box-sizing: border-box;
+}
+
+html,
+body {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+}
+
+:root {
+    --c-green-light: #e6ffe6;
+    --c-green: #e0f7e0;
+    --c-green-dark: #b2d8b2;
+    --c-green-darker: #677767;
+}
+
+body {
+    background-color: var(--c-green-light);
+    font-family: 'Montserrat', Vazir;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    margin: 0 auto;
+    min-width: 200px;
+    padding: 10px;
+    width: 100%;
+}
+
+@media (min-width: 640px) {
+    #app {
+        width: 500px;
+    }
+}
+
+.btn {
+    cursor: pointer;
+    font-family: 'Montserrat', Vazir;
+    font-weight: 500;
+}
+
+.task {
+    background-color: var(--c-green);
+    border-radius: 20px;
+    box-shadow: 0px 1px 1px 0px var(--c-green-dark);
+    display: flex;
+    padding: 15px;
+    margin-bottom: 5px;
+}
+
+.task .content {
+    flex-grow: 1;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 30px;
+    padding: 0 10px;
+    unicode-bidi: embed;
+    margin: -5px 0;
+}
+
+.task .btn-delete {
+    border: 0;
+    background-color: unset;
+    font-size: 16px;
+    padding: 0;
+    color: var(--c-green-dark);
+}
+
+.image {
+    padding: 0 15px;
+}
+
+.form {
+    padding: 10px;
+    display: flex;
+}
+
+.form .title-input {
+    border: 1px solid lightgrey;
+    border-radius: 50px;
+    display: flex;
+    flex-grow: 1;
+    align-items: center;
+    margin-right: 10px;
+    padding: 10px;
+    background-color: white;
+}
+
+.form .title-input .image img {
+    height: 40px;
+    width: 40px;
+    margin: -10px;
+}
+
+.form .title-input > input {
+    flex-grow: 1;
+    border: 0;
+    font-family: 'Montserrat', Vazir;
+}
+
+.form .title-input > input:focus {
+    outline: 0;
+}
+
+.form .btn-save {
+    background-color: var(--c-green);
+    border: 0;
+    border-radius: 50px;
+    box-shadow: 0px 1px 1px 0px var(--c-green-dark);
+    padding: 10px;
+    color: var(--c-green-darker);
+}
+
+.form .btn-save:hover {
+    background-color: var(--c-green-dark);
+}
+
+/**
+ * Utility
+ */
+
+.bold {
+    font-weight: bold;
+}
+
+.linethrough {
+    text-decoration-line: line-through;
+}
+
+.underline {
+    text-decoration-line: underline;
 }
 </style>
