303d32020d9826d821cb86b3cfabcbedd40b058b
mahdyaslami <mahdyaslami@gmail.com>
2021-11-10 06:21:47 +0330

    Update eslint config.

--- tote/.eslintrc.js
+++ tote/.eslintrc.js
@@ -5,6 +5,8 @@ module.exports = {
   },
   extends: [
     'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-strongly-recommended',
+    'plugin:vue/vue3-recommended',
     '@vue/airbnb',
   ],
   parserOptions: {
