3f75ac3fa5a56fd42c372ca11f4cf56e5a991787
mahdyaslami <mahdyaslami@gmail.com>
2021-12-10 21:32:40 +0330

    Save dist.

---
+++ dist/precache-manifest.17857ae73192eb372c2c6afa03a1037a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d09975670c0a5f9d5371",
+    "url": "/css/app.7e44284e.css"
+  },
+  {
+    "revision": "b8f9248291b0aeab1f9910e1d9fc9b61",
+    "url": "/img/writing-hand.b8f92482.gif"
+  },
+  {
+    "revision": "8aa15e70d3f66267a74472c1147aa39c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d09975670c0a5f9d5371",
+    "url": "/js/app.77e44d65.js"
+  },
+  {
+    "revision": "ebb038fa03e94045e084",
+    "url": "/js/chunk-vendors.490104f2.js"
+  },
+  {
+    "revision": "c700124cb821a2447222b9edd28d2328",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
