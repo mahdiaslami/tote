55d25d45b6602abf2158e3d3458f02f8a15e6304
mahdyaslami <mahdyaslami@gmail.com>
2021-12-10 14:26:15 +0330

    Move tote directory to root.

--- init-sw.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener("load", () => {
-    navigator.serviceWorker.register('/sw.js')
-    .then(reg => console.log("Service worker registered"))
-    .catch(err => console.error(`Service Worker Error: ${err}`));
-    });
-} else {
-    console.log("Service Worker is not supported by browser.");
-}
