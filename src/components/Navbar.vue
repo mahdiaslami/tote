fb1870bef45eb441daeb02629063ed196121dc95
mahdyaslami <mahdyaslami@gmail.com>
2022-01-05 16:24:57 +0330

    Add navbar.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,3 +1,13 @@
 <template>
-  <div>I am a navbar.</div>
+  <div class="navbar">Tote</div>
 </template>
+
+<style scoped>
+.navbar {
+    padding: 20px;
+    font-weight: 900;
+    color: var(--c-green-dark);
+    margin-bottom: 10px;
+    box-shadow: 0px 0px 5px;
+}
+</style>
