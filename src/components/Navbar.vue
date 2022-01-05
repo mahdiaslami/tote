ba4f42f5f945ac66861860da3e5d9941fa4abe0c
mahdyaslami <mahdyaslami@gmail.com>
2022-01-05 16:17:59 +0330

    Remove old files.

--- src/components/Navbar.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div>I am a navbar.</div>
-</template>
--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App'
-import './registerServiceWorker'
-import '@/assets/index.css'
-
-createApp(App).mount('#app')
