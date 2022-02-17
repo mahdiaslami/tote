33fe4b01ae0a7562583edce8e47f64b88bdbb1ad
mahdyaslami <mahdyaslami@gmail.com>
2022-02-17 17:29:40 +0330

    Formated vite.config.js.

--- vite.config.js
+++ vite.config.js
@@ -5,43 +5,43 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve:{
-    alias:{
-      '@' : path.resolve(__dirname, './src')
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
   },
   plugins: [vue(), VitePWA({
     manifest: {
-      name: "Tote",
-      short_name: "Tote",
-      start_url: "/",
-      scope_url: "/",
-      display: "standalone",
-      background_color: "#e0f7e0",
-      theme_color: "#e0f7e0",
-      orientation: "portrait",
+      name: 'Tote',
+      short_name: 'Tote',
+      start_url: '/',
+      scope_url: '/',
+      display: 'standalone',
+      background_color: '#e0f7e0',
+      theme_color: '#e0f7e0',
+      orientation: 'portrait',
       icons: [
         {
-          src: "/img/icons/icon-72x72.png",
-          type: "image/png",
-          sizes: "72x72"
+          src: '/img/icons/icon-72x72.png',
+          type: 'image/png',
+          sizes: '72x72',
         },
         {
-          src: "/img/icons/icon-96x96.png",
-          type: "image/png",
-          sizes: "96x96"
+          src: '/img/icons/icon-96x96.png',
+          type: 'image/png',
+          sizes: '96x96',
         },
         {
-          src: "/img/icons/icon-128x128.png",
-          type: "image/png",
-          sizes: "128x128"
+          src: '/img/icons/icon-128x128.png',
+          type: 'image/png',
+          sizes: '128x128',
         },
         {
-          src: "/img/icons/icon-144x144.png",
-          type: "image/png",
-          sizes: "144x144"
-        }
-      ]
-    }
-  })]
+          src: '/img/icons/icon-144x144.png',
+          type: 'image/png',
+          sizes: '144x144',
+        },
+      ],
+    },
+  })],
 })
