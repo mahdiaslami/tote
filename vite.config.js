ddb43faa2726eb52a29a700d0b832041170ff153
mahdyaslami <mahdyaslami@gmail.com>
2022-02-18 11:07:52 +0330

    Update vite.config.js.

--- vite.config.js
+++ vite.config.js
@@ -3,45 +3,48 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { VitePWA } from 'vite-plugin-pwa'
 
+const pwaOption = {
+  manifest: {
+    name: 'Tote',
+    short_name: 'Tote',
+    start_url: '/',
+    scope_url: '/',
+    display: 'standalone',
+    background_color: '#e0f7e0',
+    theme_color: '#e0f7e0',
+    orientation: 'portrait',
+    icons: [
+      {
+        src: '/img/icons/icon-72x72.png',
+        type: 'image/png',
+        sizes: '72x72',
+      },
+      {
+        src: '/img/icons/icon-96x96.png',
+        type: 'image/png',
+        sizes: '96x96',
+      },
+      {
+        src: '/img/icons/icon-128x128.png',
+        type: 'image/png',
+        sizes: '128x128',
+      },
+      {
+        src: '/img/icons/icon-144x144.png',
+        type: 'image/png',
+        sizes: '144x144',
+      },
+    ],
+  },
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [vue(), VitePWA(pwaOption)],
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+    alias: [{
+      find: '@', replacement: path.resolve(__dirname, 'src'),
+    }],
+    extensions: ['.vue', '.js'],
   },
-  plugins: [vue(), VitePWA({
-    manifest: {
-      name: 'Tote',
-      short_name: 'Tote',
-      start_url: '/',
-      scope_url: '/',
-      display: 'standalone',
-      background_color: '#e0f7e0',
-      theme_color: '#e0f7e0',
-      orientation: 'portrait',
-      icons: [
-        {
-          src: '/img/icons/icon-72x72.png',
-          type: 'image/png',
-          sizes: '72x72',
-        },
-        {
-          src: '/img/icons/icon-96x96.png',
-          type: 'image/png',
-          sizes: '96x96',
-        },
-        {
-          src: '/img/icons/icon-128x128.png',
-          type: 'image/png',
-          sizes: '128x128',
-        },
-        {
-          src: '/img/icons/icon-144x144.png',
-          type: 'image/png',
-          sizes: '144x144',
-        },
-      ],
-    },
-  })],
 })
