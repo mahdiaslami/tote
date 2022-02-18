157395d42af16183e05176f2c77ffc4be3bc008f
mahdyaslami <mahdyaslami@gmail.com>
2022-02-18 10:55:01 +0330

    Add registerSW to main.js.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import { registerSW } from 'virtual:pwa-register/vue'
+import App from './App'
 import '@/assets/index.css'
 
 createApp(App).mount('#app')
+
+registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {},
+})
