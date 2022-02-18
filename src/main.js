f0c5cd792b9add7c14c46f65a35638ed801b0c63
mahdyaslami <mahdyaslami@gmail.com>
2022-02-18 10:56:07 +0330

    Fixed unresolving ./App.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import { registerSW } from 'virtual:pwa-register/vue'
-import App from './App'
+import App from '@/App'
 import '@/assets/index.css'
 
 createApp(App).mount('#app')
