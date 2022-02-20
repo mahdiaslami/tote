b651d9836c29fc4f907f1a69743b1cadd1b55c81
mahdyaslami <mahdyaslami@gmail.com>
2022-02-20 22:47:26 +0330

    Fixed caching index.html.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 const pwaOption = {
   workbox: {
-    globPatterns: ['assets/**/*.*'],
+    globPatterns: ['**.{js,css,html}', 'assets/**/*.*'],
   },
   manifest: {
     name: 'Tote',
