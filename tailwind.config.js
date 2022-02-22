39854f211e782e1d50e8160b07a97d5e91f82636
mahdyaslami <mahdyaslami@gmail.com>
2022-02-22 22:36:57 +0330

    Colors added to tailwindcss config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     './src/**/*.{vue,js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: '#e6ffe6',
+          DEFAULT: '#e0f7e0',
+          dark: '#b2d8b2',
+          darker: '#677767',
+        },
+      },
+    },
   },
   plugins: [],
 }
