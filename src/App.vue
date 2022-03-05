ade692fe2ccda14fd941ff4f9e98d0fc0e9b9c7b
mahdyaslami <mahdyaslami@gmail.com>
2022-03-05 19:20:35 +0330

    Moved todo to new page.

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,13 @@
 <template>
   <navbar />
 
-  <div>
-    <template
-      v-for="todo in todos"
-      :key="todo.id"
-    >
-      <todo-item
-        :todo="todo"
-        :focused="(todo.id === currentId)"
-        @click="() => {select(todo.id); return false;}"
-      />
-    </template>
-
-    <div class="flex p-2.5">
-      <base-input
-        v-model="newTodo"
-        class="flex-grow"
-        @enter-keyup="saveTodo"
-      />
-
-      <base-button
-        class="px-3.5"
-        @click="saveTodo"
-      >
-        <menu-icon class="text-2xl" />
-      </base-button>
-    </div>
-  </div>
+  <Todo />
 
   <reload-prompt />
 </template>
 
 <script setup>
-import BaseButton from '@/components/BaseButton'
 import Navbar from '@/components/Navbar'
-import BaseInput from '@/components/BaseInput'
-import TodoItem from '@/components/TodoItem'
 import ReloadPrompt from '@/components/ReloadPrompt'
-import MenuIcon from '@/components/icons/MenuIcon'
-import { useTodos } from '@/hooks/useTodos'
-import { ref } from 'vue'
-
-const { todos, addTodo } = useTodos()
-
-const currentId = ref(-1)
-
-function select(id) {
-  if (currentId.value === id) {
-    currentId.value = -1
-  } else {
-    currentId.value = id
-  }
-}
-
-const newTodo = ref('')
-
-function saveTodo() {
-  addTodo(newTodo.value)
-  newTodo.value = ''
-}
+import Todo from '@/pages/Todo'
 </script>
