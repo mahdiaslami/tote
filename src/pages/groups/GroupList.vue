e20c3dcdc165cbf6457caa69f46f2af342296a05
mahdyaslami <mahdyaslami@gmail.com>
2022-03-10 19:27:18 +0330

    Updated group style.

--- src/pages/groups/GroupList.vue
+++ src/pages/groups/GroupList.vue
@@ -7,8 +7,8 @@
         class="h-20 min-w-1/2 p-1"
       >
         <div
-          class="bg-primary border border-primary-dark text-primary-darker
-          h-full p-2 rounded"
+          class="border text-primary-darker
+          h-full p-2 rounded shadow"
         >
           {{ group.title }}
         </div>
