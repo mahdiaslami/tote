c539ec84f2ee92e74c53c17ba31a68453a34bb65
mahdi.aslami <mahdi.aslami@partsoftware.com>
2023-10-16 15:48:10 +0330

    Add TestArea.vue

---
+++ src/components/TextArea.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref } from 'vue'
+
+
+defineProps({
+  modelValue: {
+    type: String,
+    default: ''
+  },
+  placeholder: {
+    type: String,
+    default: ''
+  }
+})
+
+const emit = defineEmits(['update:modelValue', 'keyup'])
+
+const p = ref(null)
+
+</script>
+
+<template>
+  <div
+    class="relative"
+  >
+    <span
+      v-show="modelValue.length === 0"
+      class="absolute text-gray-400 select-none"
+      @click="p.focus()"
+    >{{ placeholder }}</span>
+
+    <p
+      ref="p"
+      contenteditable="true"
+      class="z-10 px-1 -mx-1"
+      @input="(ev) => emit('update:modelValue', ev.target.innerText)"
+      @keyup="(ev) => emit('keyup', ev)"
+    >
+      {{ modelValue }}
+    </p>
+  </div>
+</template>
