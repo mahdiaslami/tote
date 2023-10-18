f27897a44807cd0da4af5df1bcb497796152058a
mahdi.aslami <mahdi.aslami@partsoftware.com>
2023-10-18 11:22:00 +0330

    Add panright and panleft event to Todo.vue

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,10 +1,48 @@
 <script setup>
 import TickIcon from '@/components/icons/TickIcon.vue'
+import { nextTick, onMounted, reactive, ref } from 'vue'
+import Hammer from 'hammerjs'
 
-defineProps({
+const props = defineProps({
   todo: {
     type: Object,
     required: true
+  }
+})
+
+const emit = defineEmits(['panright', 'panleft'])
+
+const div = ref(null)
+
+const data = reactive({
+  deltaX: 0,
+  transition: true,
+})
+
+onMounted(() => {
+  const hammer = new Hammer(div.value)
+
+  hammer.on('panright panleft', (ev) => {
+    data.transition = false
+    data.deltaX = ev.deltaX / 3
+
+    if (ev.isFinal) {
+      handelEnd(ev)
+    }
+  })
+
+  hammer.on('panend pancancel panup pandown', handelEnd)
+
+  function handelEnd(ev) {
+    data.transition = true
+
+    if (ev.deltaX > 100) {
+      emit('panright', props.todo)
+    } else if (ev.deltaX < -100) {
+      emit('panleft', props.todo)
+    }
+
+    nextTick(() => data.deltaX = 0)
   }
 })
 
@@ -12,12 +50,17 @@ defineProps({
 
 <template>
   <div
+    ref="div"
     class="flex flex-row items-start w-full px-1 py-4"
-    :class="{'bg-success bg-opacity-5': todo.completed_at}"
+    :class="{
+      'bg-success bg-opacity-5': todo.completed_at,
+      'transition-transform duration-100': data.transition
+    }"
+    :style="{ transform: `translate(${data.deltaX}px)` }"
   >
     <button
       class="h-6 px-2"
-      @click="todo.toggleComplete()"
+      @click.prevent="todo.toggleComplete()"
     >
       <TickIcon
         :class="[todo.completed_at ? 'text-success' : 'text-mute']"
