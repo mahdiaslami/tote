898bd1c16fd82e04b505e2c18dd281290bea8a0b
mahdi.aslami <mahdi.aslami@partsoftware.com>
2023-11-06 13:43:59 +0330

    Improve Todos.vue

--- src/pages/Todos.vue
+++ src/pages/Todos.vue
@@ -2,27 +2,38 @@
 import ArrowUpwardIcon from '@/components/icons/ArrowUpwardIcon.vue'
 import AppTextArea from '@/components/TextArea.vue'
 import Todo from '@/components/Todo.vue'
-import { reactive } from 'vue'
+import { onMounted, reactive } from 'vue'
 
 const data = reactive({
   todos: [],
   content: '',
 })
 
+onMounted(() => {
+  addTodo('سلام رفیق! من یک برنامه خیلی کوچولو برای مدیریت کار هام')
+  addTodo('هر وقت کاری خواستی اضافه کنی از پایین صفحه جایی که نوشته «کار من» می تونی اضافه کنی')
+  addTodo('وقتی کاری رو تموم کردی روی تیک سمت راستش ضربه بزن تا یک خط روش بکشم')
+  addTodo('اگر لازم شد کاری رو ویرایش کنی اونو به سمت راست هلش بده')
+  addTodo('برای حذف کار هم به سمت چپ حلش بده')
+})
+
 function save() {
   if (data.content.trim()) {
-    data.todos.push({
-      id: Date.now(),
-      completed_at: null,
-      content: data.content,
-
-      toggleComplete() {
-        this.completed_at = this.completed_at ? null : Date.now()
-      }
-    })
-
+    addTodo(data.content)
     data.content = ''
   }
+}
+
+function addTodo(content) {
+  data.todos.push({
+    id: Date.now(),
+    completed_at: null,
+    content: content,
+
+    toggleComplete() {
+      this.completed_at = this.completed_at ? null : Date.now()
+    }
+  })
 }
 
 </script>
