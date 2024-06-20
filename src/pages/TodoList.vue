1421277bdba3e5cfacd1bfae4cd928972208b046
Mahdi Aslami Khavari <mahdyaslami@gmail.com>
2024-06-21 00:18:20 +0330

    feat: update background

--- src/pages/TodoList.vue
+++ src/pages/TodoList.vue
@@ -99,7 +99,9 @@ function toggleCompleted(todo) {
 .paper {
   background:
     linear-gradient(#00000000, #00000000 23px, rgb(var(--color-line)) 24px, rgb(var(--color-line)) 24px, #00000000 25px) center top / calc(100% - 64px) 24px repeat-y,
-    linear-gradient(rgb(var(--color-primary) / 0.95), rgb(var(--color-primary) / 0.90) 100%) left top,
-    url("@/assets/pattern.png") left top repeat;
+    linear-gradient(rgba(255, 255, 255, 0.85), rgba(255, 255, 255, 0.75)),
+    url("@/assets/pattern.png") left top repeat,
+    linear-gradient(to bottom left, rgb(var(--color-info) / 0.3), rgb(var(--color-primary) / 0.8)),
+    linear-gradient(to bottom right, rgb(var(--color-success) / 0.3), rgb(var(--color-primary) / 0.8));
 }
 </style>
