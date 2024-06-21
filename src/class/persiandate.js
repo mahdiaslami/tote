6f58a15d102f476b91c1418d61afb697bad25025
Mahdi Aslami Khavari <mahdyaslami@gmail.com>
2024-06-21 08:55:25 +0330

    style: format code

--- src/class/persiandate.js
+++ src/class/persiandate.js
@@ -1,28 +1,28 @@
 
 export class PersianDate extends Date {
     getPersianFullYear() {
-        const options = {year: 'numeric'}
+        const options = { year: 'numeric' }
         const year = this.toLocaleDateString('fa-IR', options)
 
         return year
     }
 
     getPersianDate() {
-        const options = {day: 'numeric'}
+        const options = { day: 'numeric' }
         const day = this.toLocaleDateString('fa-IR', options)
 
         return day
     }
 
     getPersianMonthName() {
-        const options = { month: 'long'}
+        const options = { month: 'long' }
         const month = this.toLocaleDateString('fa-IR', options)
 
         return month
     }
 
     getPersianWeekday() {
-        const options = {weekday: 'long'}
+        const options = { weekday: 'long' }
         const weekday = this.toLocaleDateString('fa-IR', options)
 
         return weekday
