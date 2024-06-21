cb86474b22f0454808a96fbb433d3fca089f6d8e
Mahdi Aslami Khavari <mahdyaslami@gmail.com>
2024-06-21 07:35:12 +0330

    build: install swiper

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { StatusBar, Style } from '@capacitor/status-bar'
+import { register } from 'swiper/element/bundle';
 
 import App from '@/App.vue'
 import router from '@/router'
 
 import '@/assets/index.css'
+
+register();
 
 createApp(App)
   .use(createPinia())
