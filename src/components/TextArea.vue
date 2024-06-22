9098a74746298e9aab7eb70c5d201511da25e5e0
Mahdi Aslami Khavari <mahdyaslami@gmail.com>
2024-06-22 21:50:22 +0330

    style: format file

--- src/components/TextArea.vue
+++ src/components/TextArea.vue
@@ -20,22 +20,16 @@ const p = ref(null)
 </script>
 
 <template>
-  <div
-    class="relative"
-  >
-    <span
-      v-show="modelValue.length === 0"
+  <div class="relative">
+    <span v-show="modelValue.length === 0"
       class="absolute select-none text-mute"
-      @click="p.focus()"
-    >{{ placeholder }}</span>
+      @click="p.focus()">{{ placeholder }}</span>
 
-    <p
-      ref="p"
+    <p ref="p"
       contenteditable="true"
       class="z-10 px-1 -mx-1 outline-none"
       @input="(ev) => emit('update:modelValue', ev.target.innerText)"
-      @keyup="(ev) => emit('keyup', ev)"
-    >
+      @keyup="(ev) => emit('keyup', ev)">
       {{ modelValue }}
     </p>
   </div>
