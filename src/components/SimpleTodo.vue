0ffe3a91b832921302c0d0991edfdf281151010d
Mahdi Aslami Khavari <mahdyaslami@gmail.com>
2024-07-11 12:46:27 +0330

    feat: update style of mandatory todo

--- src/components/SimpleTodo.vue
+++ src/components/SimpleTodo.vue
@@ -1,15 +1,25 @@
 <script setup lang="ts">
 import TickIcon from '@/components/icons/TickIcon.vue'
 import FiberManualRecordIcon from './icons/FiberManualRecordIcon.vue';
+import type { Todo } from '@/types';
 
-defineProps({
-  todo: {
-    type: Object,
-    required: true
-  }
-})
+const props = defineProps<{
+  todo: Todo
+}>()
 
 const emit = defineEmits(['click'])
+
+function textColor() {
+  if (props.todo.type == 'mandatory') {
+    return 'text-info font-medium'
+  }
+
+  if (props.todo.completed_at) {
+    return 'text-mute'
+  } else {
+    return 'text-pen'
+  }
+}
 
 </script>
 
@@ -17,17 +27,20 @@ const emit = defineEmits(['click'])
   <div ref="container"
     class="flex flex-row items-start"
     @click.prevent="emit('click', todo)">
+
     <div class="h-6 pr-1 pl-3">
       <TickIcon v-if="todo.completed_at"
-        class="text-success" />
+        class="text-success -mt-1" />
       <FiberManualRecordIcon v-else
-        class="text-mute mt-1" />
+        :class="[todo.type == 'daily' ? 'text-mute' : 'text-info/70']" />
     </div>
+
     <p dir="auto"
       class="font-light break-words min-w-0"
-      :class="[todo.completed_at ? 'line-through text-mute' : 'text-pen']">
+      :class="[todo.completed_at ? 'line-through' : '', textColor()]">
       {{ todo.content }}
     </p>
+
     <div class="flex-shrink-0 w-10" />
   </div>
 </template>
