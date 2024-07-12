572c437754be17766834d7027f43bff96beda05c
Mahdi Aslami Khavari <mahdyaslami@gmail.com>
2024-07-12 18:09:19 +0330

    feat: add Modal.vue

---
+++ src/components/Modal.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+
+withDefaults(defineProps<{
+  cancelable: boolean
+}>(), { cancelable: true })
+
+defineOptions({
+  inheritAttrs: false
+})
+
+const model = defineModel<boolean>({ default: false })
+
+</script>
+
+<template>
+  <Teleport to="body">
+    <!-- TODO: should i add bg-black/5 to palette -->
+    <Transition name="modal">
+      <div v-if="model"
+        class="fixed top-0 h-full w-full z-10 bg-black/5"
+        @click="model = !cancelable">
+
+        <div class="bg-secondary w-11/12 absolute bottom-4 left-1/2 -translate-x-1/2
+          rounded-3xl shadow"
+          v-bind="$attrs"
+          @click="ev => ev.stopPropagation()">
+          <slot />
+        </div>
+      </div>
+    </Transition>
+  </Teleport>
+</template>
+
+<style>
+.modal-enter-active,
+.modal-leave-active,
+.modal-leave-active>div,
+.modal-enter-active>div {
+  /* TODO: use variable for all ease function animations */
+  transition-property: opacity, bottom;
+  transition-duration: 300ms;
+  transition-timing-function: cubic-bezier(0.55, 0, 0.1, 1);
+}
+
+.modal-enter-from,
+.modal-leave-to {
+  opacity: 0;
+}
+
+.modal-enter-from>div,
+.modal-leave-to>div {
+  bottom: 0px !important;
+}
+</style>
