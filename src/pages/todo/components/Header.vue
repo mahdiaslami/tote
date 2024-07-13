442a83a2e80d91bb1557750e2c1a332324207ddd
Mahdi Aslami Khavari <mahdyaslami@gmail.com>
2024-07-13 06:44:57 +0330

    fix: status bar height added to bottom of header

--- src/pages/todo/components/Header.vue
+++ src/pages/todo/components/Header.vue
@@ -19,7 +19,7 @@ defineProps({
 <template>
     <div class="flex flex-row bg-secondary border-b"
         :class="[date.isHoliday() ? 'text-danger border-danger' : 'border-success']"
-        :style="{ height: `${72 + statusBarHeight}px` }">
+        :style="{ height: `${72 + statusBarHeight}px`, paddingTop: `${statusBarHeight}px` }">
         <div class="text-4xl p-4 w-18 text-center">{{ date.getPersianDate() }}</div>
         <div class="flex flex-col justify-center flex-grow">
             <div class="my-1">{{ date.getPersianWeekday() }}</div>
