3682ae8b3e703d844145a7f80dc147540e425666
Mahdi Aslami Khavari <mahdyaslami@gmail.com>
2024-07-18 13:32:26 +0330

    feat: add app init animation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
-<script setup>
+<script setup lang="ts">
 //
 </script>
 
 <template>
-  <router-view />
+  <router-view v-slot="{ Component }">
+    <transition name="app-init">
+      <component :is="Component" />
+    </transition>
+  </router-view>
 </template>
+
+<style>
+.app-init-enter-active {
+  transition-property: opacity;
+  transition-duration: 500ms;
+  transition-timing-function: cubic-bezier(0.55, 0, 0.1, 1);
+}
+
+.app-init-enter-from {
+  opacity: 0;
+}
+</style>
