32dcbacb31dc74979f781cc0613d14b500d0870e
Mahdi Aslami Khavari <mahdyaslami@gmail.com>
2024-08-22 07:02:32 +0330

    refactor: remove --color-line

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,7 +23,6 @@ export default {
         info: 'rgb(var(--color-info) / <alpha-value>)',
         force: 'rgb(var(--color-force) / <alpha-value>)',
         danger: 'rgb(var(--color-danger) / <alpha-value>)',
-        line: 'rgb(var(--color-line) / <alpha-value>)',
         pen: 'rgb(var(--color-pen-primary) / <alpha-value>)'
       },
 
