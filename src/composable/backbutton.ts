fb6818a940851257bb2d8bf5c947d319db1bae62
Mahdi Aslami Khavari <mahdyaslami@gmail.com>
2024-09-02 07:09:13 +0330

    style: format backbutton.ts

--- src/composable/backbutton.ts
+++ src/composable/backbutton.ts
@@ -1,24 +1,24 @@
-import { App, type BackButtonListenerEvent } from "@capacitor/app"
-import { onMounted, onUnmounted } from "vue"
+import { App, type BackButtonListenerEvent } from '@capacitor/app'
+import { onMounted, onUnmounted } from 'vue'
 
 type Listener = (canGoBack: boolean) => boolean
 
 const listeners: { [tag: string]: Listener } = {}
 
 App.addListener('backButton', (ev: BackButtonListenerEvent) => {
-    let exit = true
+  let exit = true
 
-    Object.values(listeners)
-        .forEach((callback) => exit = callback(ev.canGoBack))
+  Object.values(listeners)
+    .forEach((callback) => exit = callback(ev.canGoBack))
 
-    if (exit) {
-        App.exitApp()
-    }
+  if (exit) {
+    App.exitApp()
+  }
 })
 
 export function useBackEventListener(tag: string, callback: Listener) {
-    onMounted(() => listeners[tag] = callback)
-    onUnmounted(() => delete listeners[tag])
+  onMounted(() => listeners[tag] = callback)
+  onUnmounted(() => delete listeners[tag])
 }
 
 
