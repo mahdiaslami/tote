315d886b6d63bdc3d8947faa653ce4a2ea7e9e87
Mahdi Aslami Khavari <mahdyaslami@gmail.com>
2024-11-21 08:10:00 +0330

    style: format persiondate.ts

--- src/class/persiandate.ts
+++ src/class/persiandate.ts
@@ -1,66 +1,66 @@
 
 export class PersianDate extends Date {
-    getPersianFullYear(): string {
-        const options = { year: 'numeric' } as Intl.DateTimeFormatOptions
-        const year = this.toLocaleDateString('fa-IR', options)
+  getPersianFullYear(): string {
+    const options = { year: 'numeric' } as Intl.DateTimeFormatOptions
+    const year = this.toLocaleDateString('fa-IR', options)
 
-        return year
-    }
+    return year
+  }
 
-    getPersianDate(): string {
-        const options = { day: 'numeric' } as Intl.DateTimeFormatOptions
-        const day = this.toLocaleDateString('fa-IR', options)
+  getPersianDate(): string {
+    const options = { day: 'numeric' } as Intl.DateTimeFormatOptions
+    const day = this.toLocaleDateString('fa-IR', options)
 
-        return day
-    }
+    return day
+  }
 
-    getPersianMonthName(): string {
-        const options = { month: 'long' } as Intl.DateTimeFormatOptions
-        const month = this.toLocaleDateString('fa-IR', options)
+  getPersianMonthName(): string {
+    const options = { month: 'long' } as Intl.DateTimeFormatOptions
+    const month = this.toLocaleDateString('fa-IR', options)
 
-        return month
-    }
+    return month
+  }
 
-    getPersianWeekday(): string {
-        const options = { weekday: 'long' } as Intl.DateTimeFormatOptions
-        const weekday = this.toLocaleDateString('fa-IR', options)
+  getPersianWeekday(): string {
+    const options = { weekday: 'long' } as Intl.DateTimeFormatOptions
+    const weekday = this.toLocaleDateString('fa-IR', options)
 
-        return weekday
-    }
+    return weekday
+  }
 
-    isHoliday(): boolean {
-        return this.getDay() === 5
-    }
+  isHoliday(): boolean {
+    return this.getDay() === 5
+  }
 
-    isToday(): boolean {
-        return this.toDateString() == (new Date()).toDateString()
-    }
+  isToday(): boolean {
+    return this.toDateString() == (new Date()).toDateString()
+  }
 
-    distanceInDay(date: Date): number {
-        const cur = this.duplicate().setHours(0, 0, 0, 0)
-        const today = date.setHours(0, 0, 0, 0)
+  distanceInDay(date: Date): number {
+    const cur = this.duplicate().setHours(0, 0, 0, 0)
+    const today = date.setHours(0, 0, 0, 0)
 
-        return (today - cur) / 86400000
-    }
+    return (today - cur) / 86400000
+  }
 
-    static create() {
-        return new PersianDate()
-    }
+  static create() {
+    return new PersianDate()
+  }
 
-    addDay(): this {
-        return this.addDays(1)
-    }
+  addDay(): this {
+    return this.addDays(1)
+  }
 
-    subDay(): this {
-        return this.addDays(-1)
-    }
+  subDay(): this {
+    return this.addDays(-1)
+  }
 
-    addDays(value: number): this {
-        this.setDate(this.getDate() + value)
-        return this
-    }
+  addDays(value: number): this {
+    this.setDate(this.getDate() + value)
+    return this
+  }
 
-    duplicate() {
-        return new PersianDate(this.getTime())
-    }
+  duplicate() {
+    return new PersianDate(this.getTime())
+  }
 }
