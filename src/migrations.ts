9b48cb48fe4c854450e27f60a303211e017a6cde
Mahdi Aslami Khavari <mahdyaslami@gmail.com>
2024-11-21 07:39:13 +0330

    style: format migrations.ts

--- src/migrations.ts
+++ src/migrations.ts
@@ -1,38 +1,37 @@
 
 export function migrate(): void {
-    let temp, todos, index
+  let temp, todos, index
 
-    temp = localStorage.getItem('v1/todos')
-    todos = temp ? JSON.parse(temp) : []
+  temp = localStorage.getItem('v1/todos')
+  todos = temp ? JSON.parse(temp) : []
 
-    temp = localStorage.getItem('v1/migrations/index')
-    index = temp ? parseInt(temp) : 0
+  temp = localStorage.getItem('v1/migrations/index')
+  index = temp ? parseInt(temp) : 0
 
-    if (todos) {
-        for (; index < migrations.length; index++) {
-            todos = migrations[index](todos)
-        }
+  if (todos) {
+    for (; index < migrations.length; index++) {
+      todos = migrations[index](todos)
     }
+  }
 
-    localStorage.setItem('v1/todos', JSON.stringify(todos))
-    localStorage.setItem('v1/migrations/index', `${migrations.length}`)
+  localStorage.setItem('v1/todos', JSON.stringify(todos))
+  localStorage.setItem('v1/migrations/index', `${migrations.length}`)
 }
 
 const migrations = [
-    // add date_group key
-    (todos: any) => {
-        return todos.map((el: any) => ({
-            ...el,
-            date_group: el.date_group ?? null
-        }));
-    },
+  // add date_group key
+  (todos: any) => {
+    return todos.map((el: any) => ({
+      ...el,
+      date_group: el.date_group ?? null
+    }))
+  },
 
-    // add type key
-    (todos: any) => {
-        return todos.map((el: any) => ({
-            ...el,
-            type: el.type ?? (el.date_group ? 'daily' : 'mandatory')
-        }));
-    },
-
+  // add type key
+  (todos: any) => {
+    return todos.map((el: any) => ({
+      ...el,
+      type: el.type ?? (el.date_group ? 'daily' : 'mandatory')
+    }))
+  },
 ]
