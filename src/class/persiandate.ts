45e20df9c9912012fe1f7e0686d914f3f86a8854
Mahdi Aslami Khavari <mahdyaslami@gmail.com>
2024-11-22 13:12:33 +0330

    feat: add toDateGroup and fromDateGroup

--- src/class/persiandate.ts
+++ src/class/persiandate.ts
@@ -47,6 +47,31 @@ export class PersianDate extends Date {
     return new PersianDate()
   }
 
+  toDateGroup() {
+    const options = {
+      numberingSystem: 'latn',
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit'
+    } as Intl.DateTimeFormatOptions
+
+    return this.toLocaleDateString('fa-IR', options)
+  }
+
+  static fromDateGroup(date: string) {
+    const [strYear, strMonth, strDay] = date.split('/')
+
+    const year = strYear ? Number.parseInt(strYear) : NaN
+    const month = strMonth ? Number.parseInt(strMonth) : 1
+    const day = strDay ? Number.parseInt(strDay) : 1
+
+    if (Number.isNaN(year) || Number.isNaN(month) || Number.isNaN(day)) {
+      throw `${date} is not a date group`
+    }
+
+    return new PersianDate(p2j(year, month, day))
+  }
+
   addDay(): this {
     return this.addDays(1)
   }
@@ -63,4 +88,33 @@ export class PersianDate extends Date {
   duplicate() {
     return new PersianDate(this.getTime())
   }
+}
+
+//
+// THESE CODES COPIED FROM
+// https://github.com/alibaba-aero/jalaliday/blob/master/src/calendar.js
+//
+
+// PERSIAN_EPOCH
+const PE = 1948320.5
+
+function p2j(year: number, month: number, day: number) {
+  const epbase = year - ((year >= 0) ? 474 : 473)
+  const epyear = 474 + mod(epbase, 2820)
+
+  return day +
+    ((month <= 7) ?
+      ((month - 1) * 31) :
+      (((month - 1) * 30) + 6)
+    ) +
+    $floor(((epyear * 682) - 110) / 2816) +
+    (epyear - 1) * 365 +
+    $floor(epbase / 2820) * 1029983 +
+    (PE - 1)
+}
+
+/*  MOD  --  Modulus function which works for non-integers.  */
+const $floor = Math.floor
+function mod(a: number, b: number) {
+  return a - (b * $floor(a / b))
 }
