a86122e7dbae6fd815df438e16a291cdd51939a0
Mahdi Aslami Khavari <mahdyaslami@gmail.com>
2025-02-20 18:49:49 +0330

    style: update footer border-top

--- src/pages/todo/components/Todos.vue
+++ src/pages/todo/components/Todos.vue
@@ -29,7 +29,7 @@ function handleBeforeLeave(el: any) {
 
 <template>
   <div ref="div"
-    class="paper-3 pt-5 scroll-smooth">
+    class="paper-3 pt-5 scroll-smooth shadow-inner">
     <TransitionGroup name="todo"
       :duration="animate ? 300 : 1"
       @before-leave="handleBeforeLeave">
@@ -47,31 +47,6 @@ function handleBeforeLeave(el: any) {
 </template>
 
 <style>
-.paper-1 {
-  background:
-    linear-gradient(#00000000, #00000000 23px, rgb(238 238 221) 24px, rgb(238 238 221) 24px, #00000000 25px) center top / calc(100% - 64px) 24px repeat-y,
-    linear-gradient(rgba(255, 255, 255, 0.85), rgba(255, 255, 255, 0.75)),
-    url("@/assets/pattern-2.png") left top repeat,
-    linear-gradient(to bottom left, rgb(127 127 255 / 0.3), rgb(245 249 252 / 0.8)),
-    linear-gradient(to bottom right, rgb(5 150 105 / 0.3), rgb(245 249 252 / 0.8));
-  background-attachment: local, local, local, local, local;
-}
-
-.paper-3 {
-  background:
-    linear-gradient(#00000000, #00000000 23px, theme('colors.slate.300') 24px, theme('colors.slate.300') 24px, #00000000 25px) center top / 100% 24px repeat-y,
-    linear-gradient(135deg, theme('colors.sky.50') 0%, theme('colors.sky.100') 70%, theme('colors.slate.100') 100%);
-  background-attachment: local, local, scroll;
-}
-
-.paper-2 {
-  background:
-    linear-gradient(#00000000, #00000000 23px, #CDDCFF 24px, #CDDCFF 24px, #00000000 25px) center top / calc(100% - 64px) 24px repeat-y,
-    url("@/assets/pattern-3.png") left top repeat,
-    linear-gradient(135deg, #EBFFFA 0%, #DEF0FF 70%, #F5FAFF 100%);
-  background-attachment: local, local, scroll;
-}
-
 .todo-enter-active,
 .todo-leave-active {
   transition-property: opacity, height, padding;
@@ -85,4 +60,32 @@ function handleBeforeLeave(el: any) {
   height: 0 !important;
   padding: 0 !important;
 }
+
+/** ------------------------------------------- */
+
+.paper-1 {
+  background:
+    linear-gradient(#00000000, #00000000 23px, rgb(238 238 221) 24px, rgb(238 238 221) 24px, #00000000 25px) center top / calc(100% - 64px) 24px repeat-y,
+    linear-gradient(rgba(255, 255, 255, 0.85), rgba(255, 255, 255, 0.75)),
+    url("@/assets/pattern-2.png") left top repeat,
+    linear-gradient(to bottom left, rgb(127 127 255 / 0.3), rgb(245 249 252 / 0.8)),
+    linear-gradient(to bottom right, rgb(5 150 105 / 0.3), rgb(245 249 252 / 0.8));
+  background-attachment: local, local, local, local, local;
+}
+
+.paper-2 {
+  background:
+    linear-gradient(#00000000, #00000000 23px, #CDDCFF 24px, #CDDCFF 24px, #00000000 25px) center top / calc(100% - 64px) 24px repeat-y,
+    url("@/assets/pattern-3.png") left top repeat,
+    linear-gradient(135deg, #EBFFFA 0%, #DEF0FF 70%, #F5FAFF 100%);
+  background-attachment: local, local, scroll;
+}
+/* New papers with theme colors */
+.paper-3 {
+  background:
+    linear-gradient(#00000000, #00000000 23px, theme('colors.slate.300') 24px, theme('colors.slate.300') 24px, #00000000 25px) center top / 100% 24px repeat-y,
+    linear-gradient(135deg, theme('colors.sky.50') 0%, theme('colors.sky.100') 70%, theme('colors.slate.100') 100%);
+  background-attachment: local, local, scroll;
+}
+
 </style>
